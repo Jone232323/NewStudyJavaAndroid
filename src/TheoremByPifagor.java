b5f72137d1749618990383fb7576d5e8e56f5574
Jone23 <jone23@rambler.ru>
2022-09-01 15:28:10 +0300

    add class TheoremByPifagora

---
+++ src/TheoremByPifagor.java
@@ -0,0 +1,11 @@
+public class TheoremByPifagor {
+    public static void main(String[] args) {
+
+        double a = 5;
+        double b = 5;
+
+        double c = Math.sqrt(a*a + b*b);
+
+        System.out.println("Значение гипатинузы = " + c);
+    }
+}
