32d94087dbfd20257e20447be2492bdf4dfd42c2
Jone23 <jone23@rambler.ru>
2022-09-01 14:12:31 +0300

    add class CycleFor.java

---
+++ src/CyclelFor.java
@@ -0,0 +1,10 @@
+public class CyclelFor {
+    public static void main(String[] args) {
+
+        int [] myArray = new int[]{1,2,3,4,5,6};
+
+        for (int i:myArray){
+            System.out.println(i);
+        }
+    }
+}
