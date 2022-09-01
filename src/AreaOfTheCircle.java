09439705cbb657b38a3f8617707485c4c27e28e8
Jone23 <jone23@rambler.ru>
2022-09-01 15:16:38 +0300

    add class AreaOfTheCircle

---
+++ src/AreaOfTheCircle.java
@@ -0,0 +1,10 @@
+public class AreaOfTheCircle {
+    public static void main(String[] args) {
+
+        double radiusOfTheCircles = 5.3;
+
+        double areaOfTheCircle = Math.PI*radiusOfTheCircles*radiusOfTheCircles;
+
+        System.out.println("Площадь круга = "+areaOfTheCircle);
+    }
+}
