ea0752d32e20e8e0d59a38040f8d8f85b770fed5
Jone23 <jone23@rambler.ru>
2022-09-01 10:51:39 +0300

    Start project

---
+++ src/NamesOfMyFamilyMembers.java
@@ -0,0 +1,15 @@
+public class NamesOfMyFamilyMembers {
+
+    String myName = "Женя";
+    String wifeName = "Ира";
+    String daughterName = "Катя";
+
+
+    public void printAllNames(){
+
+        System.out.println(myName);
+        System.out.println(wifeName);
+        System.out.println(daughterName);
+    }
+
+}
