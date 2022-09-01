5b2ffa4a4073d44d72a27f998cb626eafa952def
Jone23 <jone23@rambler.ru>
2022-09-01 11:37:21 +0300

    add class SecondToDays.java

--- src/Days.java
+++ src/Days.java
@@ -2,7 +2,7 @@ public class Days {
     public static void main(String[] args) {
         int days = 5000;
         int years = days / 365;
-        int month = (days % 365) / 31;
+        int month = (days % 365);
         int leftDays = days - years * 365 - month * 31;
 
         System.out.println("Общее кол-во дней: "+ days);
--- src/SecondToDays.java
+++ src/SecondToDays.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class SecondToDays {
+public class SecondToDays {
+    public static void main(String[] args) {
+        int second = 86400;
+        int minutes = second / 60 ;
+        int hours = minutes / 60;
+        int days = hours / 24;
+
+        System.out.println("Дней: " + days);
+        System.out.println("Часов: " + hours);
+        System.out.println("Минут: " + minutes);
+        System.out.println("Секунд: " + second);
+
+    }
 }
